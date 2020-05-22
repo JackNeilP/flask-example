2f04f14044ef924ee113b00fe4cda51270babba6
Jack Pridgeon <jack.pridgeon@gmail.com>
2020-05-22 19:46:27 +0100

    Created basic flask app.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+	return "Hello World"
+
+@app.route('/a')
+def goodbye_world():
+	return "Goodbye"
+
+@app.route('/hello/<name>')
+def hello_name(name):
+	return f"Hello {name}"
+
+
+if __name__ == '__main__':
+	app.run()
