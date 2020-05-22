00f4ad12dfc1a1e9e30d760608b027223081a401
Jack Pridgeon <jack.pridgeon@gmail.com>
2020-05-22 20:20:19 +0100

    Added route tests.

---
+++ tests/test_routes.py
@@ -0,0 +1,17 @@
+import unittest
+from app import app
+
+
+class RouteTest(unittest.TestCase):
+	def setUp(self):
+		self.app = app.test_client()
+		self.app.testing = True
+
+	def test_hello_name(self):
+		response = self.app.get('/hello/Fineas')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data, b'Hello Fineas')
+
+
+if __name__ == '__main__':
+	unittest.main()
