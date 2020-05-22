31db3960c39b9f23b5a78844dfbc7b35600a491f
Jack Pridgeon <jack.pridgeon@gmail.com>
2020-05-22 20:47:59 +0100

    Added test route to trigger travis build

--- app.py
+++ app.py
@@ -7,13 +7,20 @@ app = Flask(__name__)
 def hello_world():
 	return "Hello World"
 
+
 @app.route('/a')
 def goodbye_world():
 	return "Goodbye"
 
+
 @app.route('/hello/<name>')
 def hello_name(name):
 	return f"Hello {name}"
+
+
+@app.route('/b')
+def travis_check():
+	return
 
 
 if __name__ == '__main__':
